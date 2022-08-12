f5906da2a374d31d3a69906a2aa447b2214aaff5
starlabel <hello@starlabel.co>
2022-08-12 15:50:26 +0000

    python tuts - variables

---
+++ code.py
@@ -0,0 +1,32 @@
+# float
+from re import A
+
+
+x = 15
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)
+
+
+# string
+name = "Moses"
+print(name)
+
+print(name * 2)
+
+
+a = 25
+b = a
+
+print(a)
+print(b)
+
+
+b = 17
+
+print(a)
+print(b)
